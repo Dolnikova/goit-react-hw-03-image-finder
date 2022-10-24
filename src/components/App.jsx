e9a0f1fc438c0815c2ada1b699404213b3752e56
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-10-24 23:15:40 +0300

    add-isLoading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from './ImageSearch/ImageGallery/ImageGallery';
 import Searchbar from './ImageSearch/Searchbar';
@@ -8,13 +9,15 @@ import Modal from './Modal/Modal';
 
 export default class App extends Component {
   state = {
-    images: null,
+    images: [],
     page: 1,
     search: '',
     error: null,
     showModal: false,
     currentImageUrl: null,
     currentImageDescription: null,
+    totalHits: null,
+    isLoading: false,
   };
 
   handleSubmit = search => {
@@ -44,26 +47,45 @@ export default class App extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
+    if (prevState.search !== this.state.search) this.setState({ page: 1 });
     if (
       prevState.search !== this.state.search ||
       prevState.page !== this.state.page
     ) {
+      this.setState({ isLoading: true });
       fetch(
         `https://pixabay.com/api/?q=${this.state.search}&page=${this.state.page}&key=29818369-26f2aeadd77818b7a67304d74&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(res => res.json())
-        .then(images => this.setState({ images }))
-        .catch(error => this.setState({ error }));
+        .then(images => {
+          if (images.totalHits === 0) {
+            this.setState({ images: [], totalHits: 0 });
+            return Promise.reject(
+              toast.error(`There is no image with name ${this.state.search}`)
+            );
+          }
+          this.setState({ images: images.hits, totalHits: images.totalHits });
+        })
+        .catch(error => this.setState({ error }))
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
     }
   }
 
   render() {
-    const { images, showModal, currentImageUrl, currentImageDescription } =
-      this.state;
+    const {
+      images,
+      totalHits,
+      page,
+      showModal,
+      currentImageUrl,
+      currentImageDescription,
+    } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleSubmit} />
-        {images && (
+        {!!images.length && (
           <>
             {console.log(currentImageUrl)}
             {showModal && (
@@ -73,8 +95,10 @@ export default class App extends Component {
                 alt={currentImageDescription}
               />
             )}
-            <ImageGallery hits={images.hits} openModal={this.openModal} />
-            <Button onClick={this.nextPage}></Button>
+            <ImageGallery hits={images} openModal={this.openModal} />
+            {totalHits - page * 12 > 0 && (
+              <Button onClick={this.nextPage}></Button>
+            )}
           </>
         )}
 
